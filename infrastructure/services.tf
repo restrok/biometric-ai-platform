124f86296165ecd190aaf977cd544f018d3bbe55
Restrok <fedeale.sirio@gmail.com>
2026-05-15 15:56:21 -0300

    tf format

--- infrastructure/services.tf
+++ infrastructure/services.tf
@@ -3,12 +3,12 @@
 
 locals {
   services = [
-    "serviceusage.googleapis.com",      # Necesario para cuotas y límites
-    "bigquery.googleapis.com",          # Motor de análisis
-    "storage.googleapis.com",           # Almacenamiento de artefactos
-    "secretmanager.googleapis.com",      # Gestión de llaves y tokens
-    "billingbudgets.googleapis.com",     # Necesario para las alertas de presupuesto
-    "iam.googleapis.com",               # Gestión de identidades y permisos
+    "serviceusage.googleapis.com",   # Necesario para cuotas y límites
+    "bigquery.googleapis.com",       # Motor de análisis
+    "storage.googleapis.com",        # Almacenamiento de artefactos
+    "secretmanager.googleapis.com",  # Gestión de llaves y tokens
+    "billingbudgets.googleapis.com", # Necesario para las alertas de presupuesto
+    "iam.googleapis.com",            # Gestión de identidades y permisos
   ]
 }
 
