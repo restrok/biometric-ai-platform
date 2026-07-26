fec335ab2acabf92a73df5acd2f7f6378474e09b
Restrok <fedeale.sirio@gmail.com>
2026-07-26 19:18:58 -0300

    infra(terraform): declare and import BigQuery quota preference by explicit ID

--- infrastructure/modules/bigquery/main.tf
+++ infrastructure/modules/bigquery/main.tf
@@ -6,4 +6,15 @@ resource "google_bigquery_dataset" "biometric_dataset" {
   delete_contents_on_destroy = true
 }
 
-
+# 🛡️ Quota Management to Protect Free Tier
+# Manages the existing QueryUsagePerDay quota preference in GCP
+resource "google_cloud_quotas_quota_preference" "bigquery_query_usage" {
+  name                 = "654e8a37-cc6a-42a5-b3b7-2d25f2a3fc97"
+  service              = "bigquery.googleapis.com"
+  parent               = "projects/${var.project_id}"
+  quota_id             = "QueryUsagePerDay"
+  ignore_safety_checks = "QUOTA_DECREASE_PERCENTAGE_TOO_HIGH"
+  quota_config {
+    preferred_value = 34952
+  }
+}
